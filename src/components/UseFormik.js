41f9658959ff4f94940d6891655a061c87b9a5b6
dongoc <dongoc21hj@gmail.com>
2020-10-18 11:13:11 +0900

    useFormik : basic validate

--- src/components/UseFormik.js
+++ src/components/UseFormik.js
@@ -6,6 +6,7 @@ const UseFormik = () => {
   // TITLE useFormik returns object
   // 1. initialValues -> 각 input의 handleChange를 통해 formik.values에 반영된다
   // 2. onSubmit -> form의 handleSubmit을 통해 onSubmit 실행. button에 type="submit" 추가
+  // 3. validate
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -14,6 +15,21 @@ const UseFormik = () => {
     },
     onSubmit: (values) => {
       console.log('onSubmit', values);
+    },
+    validate: (values) => {
+      let errors = {};
+      // values.name values.email values.channel
+      // errors.name errors.email errors.channel
+      if (!values.name) {
+        errors.name = 'Required';
+      }
+      if (!values.email) {
+        errors.email = 'Required';
+      }
+      if (!values.channel) {
+        errors.channel = 'Required';
+      }
+      return errors;
     }
   })
 
