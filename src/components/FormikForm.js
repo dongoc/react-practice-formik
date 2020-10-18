b47d0e5b3e82eb6480df11df6a2f03c634eee43c
dongoc <dongoc21hj@gmail.com>
2020-10-18 12:04:04 +0900

    formikForm : Field component-more detail(props)

--- src/components/FormikForm.js
+++ src/components/FormikForm.js
@@ -9,12 +9,14 @@ import * as Yup from 'yup';
 // - Field <- replace input
 // - Error Messages
 
-const UseFormik = () => {
+const FormkiForm = () => {
 
   const initialValues = {
       name: '',
       email: '',
-      channel: ''
+      channel: '',
+      comments: '',
+      address: '',
   }
   const onSubmit = (values) => {
       console.log('onSubmit', values);
@@ -33,23 +35,47 @@ const UseFormik = () => {
       onSubmit={onSubmit}
     >
       <Form>
-        <div className="form-contril">
+        <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name"/>
           <ErrorMessage name='name' />
         </div>
         
-        <div className="form-contril">
-          <label htmlFor="name">E-mail</label>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email" />
           <ErrorMessage name='email' />
         </div>
 
-        <div className="form-contril">
-          <label htmlFor="name">Channel</label>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
           <ErrorMessage name='channel' />
         </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+          <ErrorMessage name='comments' />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              console.log(props);
+              const { field, form, meta } = props;
+              return (
+                <div>
+                  <input id='address' {...field}/>
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              )
+            }}
+          </Field>
+          <ErrorMessage name='comments' />
+        </div>
+
 
         <button type="submit">Submit</button>
       </Form>
@@ -57,4 +83,4 @@ const UseFormik = () => {
   )
 }
 
-export default UseFormik;
+export default FormkiForm;
